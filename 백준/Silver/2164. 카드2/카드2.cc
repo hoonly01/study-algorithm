c5dda96f58bbff11e396679b888f335d030fcb10
Jonghoon Hwang <97182418+hoonly01@users.noreply.github.com>
2024-04-13 16:47:23 +0900

    [Silver IV] Title: 카드2, Time: 8 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+
+    queue<int> deck;
+    int num, t;
+
+    cin >> num;
+
+    for(int i=1; i<=num; i++) {
+        deck.push(i);
+    }
+    while( deck.size() != 1 ) {
+        deck.pop();
+        deck.push(deck.front());
+        deck.pop();
+    }
+    cout << deck.front();
+
+    return 0;
+}
