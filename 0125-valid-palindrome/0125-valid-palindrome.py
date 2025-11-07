e3331a28ce1b972eef85d1b12431f2b9b3029ae4
Jonghoon Hwang <97182418+hoonly01@users.noreply.github.com>
2025-11-08 00:45:11 +0900

    Delete 0125-valid-palindrome directory

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s1 = ""
-        s = s.lower()
-        for i in s:
-            if(i.isalnum()):
-                s1 = s1 + i
-        s2 = s1[::-1]
-        return s1 == s2
