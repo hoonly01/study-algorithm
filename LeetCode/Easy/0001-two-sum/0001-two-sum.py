ae0d37b26758366f008632bc73f42069e5d6a7be
Jonghoon Hwang <97182418+hoonly01@users.noreply.github.com>
2026-04-18 20:50:27 +0900

    Time: 321 ms (31.78%), Space: 19.9 MB (69.97%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if(nums[i] + nums[j] == target):
-                    ans.append(i)
-                    ans.append(j)
-                    return ans
+        for i, n in enumerate(nums):
+            complement = target - n
+            if complement in nums[i+1:]:
+                result = [i, nums[i+1:].index(complement)+i+1]
+        return result
+
+"""
+target - i를 만들고 배열에 있는지 검색
+"""
