d8341135faa98b8d946618eb85cee392e53bfb7b
Jonghoon Hwang <97182418+hoonly01@users.noreply.github.com>
2026-04-18 22:40:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0561-array-partition/0561-array-partition.py
+++ LeetCode/Easy/0561-array-partition/0561-array-partition.py
@@ -1,7 +1,21 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        ans = 0
         nums.sort()
-        for i in range(0, len(nums)//2):
-            ans += nums[i*2]
+        ans = sum(nums[::2])
         return ans
+
+"""
+v1
+짝수번째 값 계산
+
+ans = 0
+nums.sort()
+for i in range(0, len(nums)//2):
+    ans += nums[i*2]
+return ans
+"""
+
+"""
+v2 pythonic way
+slicing
+"""
