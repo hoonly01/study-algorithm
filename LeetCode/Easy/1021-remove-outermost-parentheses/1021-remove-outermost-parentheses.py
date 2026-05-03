5d1100c0827564bc0447a25fff4e995da1564aef
Jonghoon Hwang <97182418+hoonly01@users.noreply.github.com>
2026-05-04 02:16:59 +0900

    Time: 0 ms (100%), Space: 19.4 MB (9.43%) - LeetHub

--- LeetCode/Easy/1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ LeetCode/Easy/1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,6 +1,26 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        comp = ""
+        ans = []
+        depth = 0
+
+        for ch in s:
+            if ch == '(':
+                if depth > 0:       # ( 가 나왔지만 depth가 1이상, 즉 진짜 composition이어야 append
+                    ans.append(ch)  
+                depth += 1
+            else:
+                depth -= 1
+                if depth > 0:        # ) 가 나왔다. 일단 depth를 줄여보고, 0이면 composition이 아니니 pass
+                    ans.append(ch)
+        
+        ans = ''.join(ans)
+        return ans
+
+"""
+v1
+열린 개수를 세고 0이 되면 거기까지가 composition
+
+comp = ""
         ans = ""
         cnt = 0
         
@@ -16,7 +36,11 @@ class Solution:
                 ans += comp[1:len(comp)-1]
                 comp = ""
         return ans
+"""
 
 """
-열린 개수를 세고 0이 되면 거기까지가 composition
+풀이 후 공부
+열린 개수를 세고 0이 되면 -> 접근은 맞다. 그렇지만 그 동안의 과정을 문자열로 다 만들 필요는 없다.
+즉 depth가 0일 때를 얻으면 된다는 것
+그 때의 idx만 가지고 str를 만들면 됨
 """
